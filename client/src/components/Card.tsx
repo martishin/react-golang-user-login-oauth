960a570dc375b6b522e15e5b872bb17dfa544a50
Alex <asmartishin@gmail.com>
2024-11-21 10:23:26 +0100

    Update Card.tsx

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -14,7 +14,7 @@ const Card: React.FC<CardProps> = ({ children }) => {
         <p>
           Check out the code on{" "}
           <a
-            href="https://github.com/martishin/react-golang-oauth"
+            href="https://github.com/martishin/react-golang-oauth-login"
             target="_blank"
             className="underline text-gray-500 hover:text-gray-700"
           >
